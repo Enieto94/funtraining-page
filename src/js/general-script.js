15f526f64828f11d7fb73ca063a4d591982c7c75
edwinNieto <egnieto94@gmail.com>
2021-02-13 03:16:24 -0500

    https api

--- src/js/general-script.js
+++ src/js/general-script.js
@@ -1,6 +1,6 @@
 function ApiCall(endpointUri, httpMethod, dataToSend) {
 	// const API_URL = 'http://localhost:8000/api';
-	const API_URL = 'http://api.funtraining.net/api';
+	const API_URL = 'https://api.funtraining.net/api';
 
 	return axios.post(`${API_URL}${endpointUri}`, dataToSend)
 		.then((response) => {
